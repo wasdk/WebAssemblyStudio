8ab340eadb0381fe93163684aadac529c8db76e0
nilesh <nileshgulia@gmail.com>
2018-02-04 15:02:38 +0530

    format code

--- tests/Button/Button.spec.tsx
+++ tests/Button/Button.spec.tsx
@@ -1,21 +1,19 @@
 import * as React from "react";
-import { shallow } from "enzyme";
-import  {Button} from "../../src/components/Button"
+import {shallow} from "enzyme";
+import {Button} from "../../src/components/Button"
 
-describe("Tests for button component",() =>{
-    let setup = (value) => {
-        const props ={
-            isDisabled: value,
-            onClick: () => {},
-            icon: <button></button>,
-            label : "",
-            title : "",
-
-        }
-        return shallow(<Button {...props}/>)
+describe("Tests for button component", () => {
+  let setup = (value) => {
+    const props = {
+      isDisabled: value,
+      onClick: () => {},
+      icon: <button></button>,
+      label: "",
+      title: ""
     }
-it("Button renders correctly", () => {
-let wrapper = setup(false)
-}
-)
+    return shallow(<Button {...props}/>)
+  }
+  it("Button renders correctly", () => {
+    let wrapper = setup(false)
+  })
 })
