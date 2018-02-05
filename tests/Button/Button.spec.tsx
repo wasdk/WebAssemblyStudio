29a5d80431d0e41f8f1bfc76313a652f0f0db367
Yury Delendik <ydelendik@mozilla.com>
2018-02-05 15:00:38 -0600

    Add license headers.

--- tests/Button/Button.spec.tsx
+++ tests/Button/Button.spec.tsx
@@ -1,3 +1,6 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
 import * as React from "react";
 import {shallow} from "enzyme";
 import {Button} from "../../src/components/Button";
