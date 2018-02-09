6db9ee74adeaafef026cdcb8014ba10899082798
Michael Bebenita <mbebenita@gmail.com>
2018-02-08 17:26:05 -0800

    Disable hello_world C template for now since it does not work.

--- templates/templates.js
+++ templates/templates.js
@@ -60,37 +60,4 @@
       ["README.md"]
     ]
   }
-},
-{
-  "name": "C Hello World",
-  "description": `# Hello World in C
-Creates a C project that prints "Hello World" using a minimal POSIX API.
-`,
-  "icon": "svg/file_type_c.svg",
-  "project": {
-    "name": "Project",
-    "directory": "hello_world_c",
-    "children": [{
-      "name": "src",
-      "children": [{
-        "name": "main.c",
-        "type": "c"
-      }, {
-        "name": "main.js",
-        "type": "javascript",
-      }, {
-        "name": "main.html",
-        "type": "html",
-      }]
-    }, {
-      "name": "build.ts",
-      "type": "typescript",
-    }, {
-      "name": "README.md",
-      "type": "markdown",
-    }],
-    "openedFiles": [
-      ["README.md"]
-    ]
-  }
 }]
