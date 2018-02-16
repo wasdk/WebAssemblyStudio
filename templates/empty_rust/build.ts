d93a07f372eddecec473708a84d5a38e7afebec2
Michael Bebenita <mbebenita@gmail.com>
2018-02-15 17:04:23 -0800

    Add transient file flag, and prevent transient files from being saved in projects.

--- templates/empty_rust/build.ts
+++ templates/empty_rust/build.ts
@@ -1,6 +1,6 @@
 gulp.task("build", async () => {
   const data = await Service.compileFile(project.getFile("src/main.rs"), "rust", "wasm", "-g -O3");
-  const outWasm = project.newFile("out/main.wasm", "wasm");
+  const outWasm = project.newFile("out/main.wasm", "wasm", true);
   outWasm.setData(data);
 });
 
--- templates/empty_ts/setup.js
+++ templates/empty_ts/setup.js
@@ -22,7 +22,7 @@ require(["assemblyscript/bin/asc"], asc => {
       writeFile: (filename, contents) => {
         const name = filename.startsWith("/") ? filename.substring(1) : filename;
         const type = filetypeForExtension(name.substring(name.lastIndexOf(".") + 1));
-        project.newFile(name, type).setData(contents);
+        project.newFile(name, type, true).setData(contents);
       },
       listFiles: (dirname) => []
     }, fn);
