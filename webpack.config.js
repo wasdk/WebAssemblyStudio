b7784053a7e068fcdcbfb76448c712eab0fd9f23
Anshul <malikanshul29@gmail.com>
2018-02-24 00:59:07 +0530

    Generate production build on npm run build

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,52 @@
 const path = require("path");
+const Uglify = require("uglifyjs-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-      filename: "[name].bundle.js",
-      chunkFilename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist/"),
-      publicPath: "/dist/",
-  },
+module.exports = env => {
+  const isProduction = env.prod;
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  const config = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "[name].bundle.js",
+        chunkFilename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+    },
 
-  resolve: {
-      // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: [".ts", ".tsx", ".js", ".json"]
-  },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
 
-  module: {
-      rules: [
-          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
 
-          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-      ]
-  },
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-      "react": "React",
-      "react-dom": "ReactDOM",
-      "monaco-editor": "undefined"
-  },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM",
+        "monaco-editor": "undefined"
+    },
+    plugins: []
+  };
+
+  if (isProduction) {
+      console.log("Using production build");
+      config.plugins.push(new Uglify());
+  }
+
+  return config;
 };
