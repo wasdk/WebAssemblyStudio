3f2ec5c8651e97284bd086fae6b9f77a22f10c13
Yury Delendik <ydelendik@mozilla.com>
2018-05-15 16:45:48 -0500

    Replace "awesome-typescript-loader" with "ts-loader"
    ... due to https://github.com/s-panferov/awesome-typescript-loader/issues/541

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = env => {
     module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            { test: /\.tsx?$/, loader: "ts-loader" },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
