007f383744aa0cc16cb1178028bf5e95d73ccde4
Ashley Williams <ashley666ashley@gmail.com>
2018-06-02 10:13:17 +0200

    fix(arc): improve rust template

--- misc/arc-templates/arc_rust/src/color.rs
+++ misc/arc-templates/arc_rust/src/color.rs
@@ -1,17 +1,19 @@
 #[repr(C)]
 #[derive(Copy)]
 pub struct Rgb {
-  pub r: u8,
-  pub g: u8,
-  pub b: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
 }
 
 impl Rgb {
-  pub fn new(r: u8, g: u8, b: u8) -> Rgb {
-    Rgb {r, g, b}
-  }
+    pub fn new(r: u8, g: u8, b: u8) -> Rgb {
+        Rgb { r, g, b }
+    }
 }
 
 impl Clone for Rgb {
-  fn clone(&self) -> Rgb { *self }
+    fn clone(&self) -> Rgb {
+        *self
+    }
 }
--- misc/arc-templates/arc_rust/src/lib.rs
+++ misc/arc-templates/arc_rust/src/lib.rs
@@ -1,26 +1,31 @@
 static mut module_instance: *mut ArcModule = 0 as *mut ArcModule;
 
-mod color;
 mod arc_module;
+mod color;
 mod utils;
 
-use color::Rgb;
 use arc_module::ArcModule;
+use color::Rgb;
 
-pub use utils::init;
 pub use utils::getAnimationBuffer;
+pub use utils::init;
 
 #[no_mangle]
-pub extern fn apply() {
-  let mut module = ArcModule::get_instance();
-  let rows = module.rows;
-  let cols = module.cols;
-  let ref mut animation = module.get_animation().as_mut_slice();
-  for (index, frame) in animation.chunks_mut(rows * cols).enumerate() {
-    for row in 0 .. rows {
-      for col in 0 .. cols {
-        frame[row * cols + col] = Rgb::new(row as u8 * 5, col as u8 * 5, ((index * 5) % 0xffusize) as u8);
-      }
+pub extern "C" fn apply() {
+    let mut module = ArcModule::get_instance();
+    let rows = module.rows;
+    let cols = module.cols;
+
+    let animation = module.get_animation().as_mut_slice();
+    for (index, frame) in animation.chunks_mut(rows * cols).enumerate() {
+        for row in 0..rows {
+            for col in 0..cols {
+                frame[row * cols + col] = Rgb::new(
+                    row as u8 * 5,
+                    col as u8 * 5,
+                    ((index * 5) % 0xffusize) as u8,
+                );
+            }
+        }
     }
-  }
 }
