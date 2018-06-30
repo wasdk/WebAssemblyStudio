c769c34e15b53c805c21e3dda9797c58d579903a
MaxGraey <maxgraey@gmail.com>
2018-06-30 15:02:12 +0300

    Update AssemblyScript template

--- templates/empty_ts/src/main.js
+++ templates/empty_ts/src/main.js
@@ -1,14 +1,17 @@
 WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
   env: {
-    sayHello: function() {
+    sayHello: function () {
       console.log("Hello from WebAssembly!");
     },
-    abort: function(msg, file, line, column) {
+    abort: function (msg, file, line, column) {
       console.error("abort called at main.ts:" + line + ":" + column);
     }
+  },
+  console: {
+    logi: function (value) { console.log('logi: ' + value); },
+    logf: function (value) { console.log('logf: ' + value); }
   }
 }).then(result => {
   const exports = result.instance.exports;
   document.getElementById("container").innerText = "Result: " + exports.add(19, 23);
 }).catch(console.error);
-
