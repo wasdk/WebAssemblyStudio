57aa9aa0135d648b5e5196ff48d8c99994cee27c
MaxGraey <maxgraey@gmail.com>
2018-07-04 19:01:02 +0300

    minor refactoring

--- templates/empty_ts/src/main.js
+++ templates/empty_ts/src/main.js
@@ -1,15 +1,15 @@
 WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
   env: {
-    sayHello: function () {
+    sayHello() {
       console.log("Hello from WebAssembly!");
     },
-    abort: function (msg, file, line, column) {
+    abort(msg, file, line, column) {
       console.error("abort called at main.ts:" + line + ":" + column);
     }
   },
   console: {
-    logi: function (value) { console.log('logi: ' + value); },
-    logf: function (value) { console.log('logf: ' + value); }
+    logi(value) { console.log('logi: ' + value); },
+    logf(value) { console.log('logf: ' + value); }
   }
 }).then(result => {
   const exports = result.instance.exports;
